99cfee1de003b3c3b759645b38daa8d6fff23563
hulmea <hulmeag@gmail.com>
2014-10-26 18:13:36 +0000

    Adding installer to setup temp probes

---
+++ LickerTank/src/com/install/Install.java
@@ -0,0 +1,34 @@
+package com.install;
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class Install {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		File f = new File("properties.txt");
+		 
+		  if(f.exists()){
+			  System.out.println("File existed");
+			  
+					  
+			  
+			  
+			  
+		  }
+		  
+		  else{
+			  
+			  
+			  
+			  
+			  
+			  System.out.println("File not found!");
+		  }
+		
+		
+		
+	}
+
+}
